a81e90fb6afb819e4816f56b946116758e141c85
sherin shidinas <sherinshidinas615@gmail.com>
2024-05-25 00:46:59 +0530

    frontend URL added

--- server/server.js
+++ server/server.js
@@ -17,7 +17,22 @@ dotenv.config();
 
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+// Use environment variable to set the allowed origin
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:3000"];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
+
+// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 const connect = async () => {
   try {
